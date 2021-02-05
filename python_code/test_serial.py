ffd068211265ec2bacc99e0fcf2a384680009187
thatITfox <78403482+thatITfox@users.noreply.github.com>
2021-02-05 10:40:25 +0900

    this is the serial reader
    this python serial reader is for a computer and not edited for the raspberry pi. i will be puting the other code for the raspberry pi here to

---
+++ python_code/test_serial.py
@@ -0,0 +1,12 @@
+import serial
+
+if __name__ == '__main__':
+    ser = serial.Serial('com3', 9600, timeout=1)
+    ser.flush()
+while True:
+    with open('serial_save.txt', 'wb') as data:
+        data_data = data
+    if ser.in_waiting > 0:
+        line = ser.readline().decode('utf-8').rstrip()
+        # data_data.write(bytes(line))
+        print(line)
