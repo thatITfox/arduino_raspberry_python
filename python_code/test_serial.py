30945f4e6710ff2efd0803a40ab44fb75aa2c3b2
thatITfox <78403482+thatITfox@users.noreply.github.com>
2021-02-18 20:46:22 +0700

    Update test_serial.py

--- python_code/test_serial.py
+++ python_code/test_serial.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     ser = serial.Serial('com3', 9600, timeout=1)
     ser.flush()
 while True:
-    with open('serial_save.txt', 'wb') as data:
+    with open('serial_save.txt', 'w') as data:
         data_data = data
     if ser.in_waiting > 0:
         line = ser.readline().decode('utf-8').rstrip()
