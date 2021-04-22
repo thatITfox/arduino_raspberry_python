ba131ecc795cf451e1ef81603db538a918904469
thatITfox <vajra.wiataya@gmail.com>
2021-04-22 20:26:37 +0700

    added an attack file, this code is still under testing can't be used right now

---
+++ python_code/attack_ir.py
@@ -0,0 +1,16 @@
+import serial
+attack = True
+if __name__ == '__main__':
+    ser = serial.Serial('com5', 9600, timeout=1)
+    ser.flush()
+
+while attack:
+        send_yn = input("do you wanna attack:")
+        if send_yn == "Y":
+            attack_now = input("send the serial code to attack:")
+            ser.write(bytes(attack_now, 'utf-8'))
+            # ser.write(b'3')
+            print(attack_now)
+        else:
+            print("thankyou")
+            attack = False
