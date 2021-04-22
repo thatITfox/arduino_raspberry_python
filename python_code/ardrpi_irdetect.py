77e6c9b1b063ba78c6d5a39b68e03b940b4b46ac
thatITfox <vajra.wiataya@gmail.com>
2021-04-22 20:25:22 +0700

    make the code to append the file instead of just overwriting it

--- python_code/ardrpi_irdetect.py
+++ python_code/ardrpi_irdetect.py
@@ -4,7 +4,7 @@ if __name__ == '__main__':
     ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
     ser.flush()
 while True:
-    with open('serial_save.txt', 'w') as data:
+    with open('serial_save.txt', 'a') as data:
         if ser.in_waiting > 0:
             line = ser.readline().decode('utf-8').rstrip()
             data.write(line)
