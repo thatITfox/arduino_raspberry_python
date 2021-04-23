0a8322c82c347a662c2401ae4827b244fa23cbdb
thatITfox <vajra.wiataya@gmail.com>
2021-04-23 14:02:24 +0700

    new feature, now you can add your own limit

--- python_code/ardrpi_irdetect.py
+++ python_code/ardrpi_irdetect.py
@@ -1,13 +1,20 @@
 import serial
 
 if __name__ == '__main__':
-    ser = serial.Serial('com5', 9600, timeout=1)
+    try:
+        # for raspberry pi
+        ser = serial.Serial('/dev/ttyS0')
+    except:
+        # for a satndard desktop or laptop, usualy for testing
+        ser = serial.Serial('com5', 9600, timeout=1)
     ser.flush()
 List = []
 read = True
+print("test is now on!!")
+jumlah_deteksi = int(input("input how many remote detection u want:"))
 while read:
-    with open('serial_save.txt', 'a') as data:
-        if len(List) <= 10:
+    with open('data_dan_info.txt', 'a') as data:
+        if len(List) <= jumlah_deteksi:
             if ser.in_waiting > 0:
                 line = ser.readline().decode('utf-8').rstrip()
                 List.append(line)
@@ -17,4 +24,3 @@ while read:
                 data.write(i)
                 data.write("\n")
             read = False
-
