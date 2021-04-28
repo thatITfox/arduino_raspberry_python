62af726682722655d39cd4c427cfa6b461ee783d
thatITfox <vajra.wiataya@gmail.com>
2021-04-28 15:00:37 +0700

    added a read command so it would read the output

--- python_code/attack_ir.py
+++ python_code/attack_ir.py
@@ -1,4 +1,5 @@
 import serial
+import time
 attack = True
 if __name__ == '__main__':
     try:
@@ -14,8 +15,11 @@ while attack:
     if send_yn == "Y":
         attack_now = input("send the serial hex code to attack:")
         ser.write(bytes(attack_now, 'utf-8'))  # under testing and
+        time.sleep(0.5)
+        make_sure = ser.read()
+        print(make_sure)
         # ser.write(b'3')
-        print(attack_now)
+        # print(attack_now)
     else:
         print("thankyou")
         attack = False
