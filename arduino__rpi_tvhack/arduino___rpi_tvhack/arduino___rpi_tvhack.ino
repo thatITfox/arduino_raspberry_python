ae09e797106bb00f5dbb95c9b4d1992d348ee97a
thatITfox <vajra.wiataya@gmail.com>
2021-05-08 13:47:04 +0700

    decode the coming hex code and added an led to see if the arduino can detect the serial code

--- arduino__rpi_tvhack/arduino___rpi_tvhack/arduino___rpi_tvhack.ino
+++ arduino__rpi_tvhack/arduino___rpi_tvhack/arduino___rpi_tvhack.ino
@@ -2,11 +2,12 @@
 // percobaan percobaan lain nya akan di uji pada kode ini
 
 #include <IRremote.h>
+
 #define led 13
-const int RECV_PIN = 2;  // pin penerima inframerah
+const int RECV_PIN = 2;  // pin penerima inframerah 
 char raspberry_pi_info;
 String decoded_resultes;
-String data_need_tosend;
+int data_need_tosend = 0;
 IRsend irsend;
 IRrecv irrecv(RECV_PIN);
 decode_results results;
@@ -22,20 +23,22 @@ void setup() {
 void loop() {
   if (irrecv.decode(&results)){
     // Print Code in HEX
-        Serial.println(results.value, HEX);
+        Serial.println(results.value, HEX); 
         irrecv.resume();
   }
-   if (Serial.available()) { //detect the raspberry pi serial code
-      data_need_tosend = Serial.readStringUntil('\n'); // recive and read the data from raspberry pi
+   if (Serial.available() > 0) { //detect the raspberry pi serial code 
+      // data_need_tosend = Serial.readStringUntil('\n'); // recive and read the data from raspberry pi
       // if (data_need_tosend[0] == "r"){
       // data_need_tosend[0] = '\0';
       // irsend.sendNEC(data_need_tosend, 32); // proses penyaerangan nya entar di tambahi kode hex nya lewat variabe
-      Serial.println(data_need_tosend);
+      // Serial.println(data_need_tosend);
+      data_need_tosend = Serial.read();
+      Serial.println(data_need_tosend, HEX);
       digitalWrite(led, HIGH);
       delay(200);
       digitalWrite(led, LOW);
-   }
-
+   } 
+  
     // Add a small delay before repeating
     delay(200);
 }
